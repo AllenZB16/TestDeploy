59c17be791c556d89691f78c2c3336b7a72932e3
AllenZB16 <allen.zhangbo@tron.network>
2024-12-11 15:14:33 +0800

    add file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App!</h1>
+      <p>Edit <code>src/App.js</code> and save to reload.</p>
+    </div>
+  );
+}
+
+export default App;
